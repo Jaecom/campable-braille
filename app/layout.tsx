797da4925febc4731481585eaf65ab49286f71bc
Jaecom <jae.yoon7373@gmail.com>
2025-08-24 13:24:42 +0900

    fix: change web name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-	title: "Sunny Braille",
-};
+	title: "Braille Camp",
+}
 
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode;
+	children: React.ReactNode
 }>) {
 	return (
 		<html lang="en">
@@ -20,5 +20,5 @@ export default function RootLayout({
 				{children}
 			</body>
 		</html>
-	);
+	)
 }
